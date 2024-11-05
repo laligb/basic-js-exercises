2bf3479e48801158079b30e17a4e764049b9dee0
Lali Bibilashvili <lalibibilashvili@gmail.com>
2024-11-05 16:17:36 +0100

    exercise 1.4 level 2 done

---
+++ exercise1.4/level2.js
@@ -0,0 +1,26 @@
+/**
+ * Exercise 3
+ * Copying objects with Spread: Create an object 'object1'. It then creates a
+ * second object, 'object2', which is a copy of 'object1' using the spread
+ * operator. Change a property of 'object2' and check that 'object1'
+ * has not changed.
+ */
+
+// Create an Object
+export const object1 = { name: "object 1" };
+export const object2 = { ...object1 };
+
+/**
+ * Exercise 4
+ * Rest in Destructuring: Create an array with several elements.
+ * It uses destructuring and the remainder operator to assign the first two
+ * elements to variables, and then assigns the rest of the elements to a third
+ * variable.
+ */
+export function useRestDestructor(array) {
+  const [first, second, ...third] = array;
+  let var1 = first;
+  let var2 = second;
+  let var3 = third;
+  console.log(`First: ${first}, Second: ${second}, Third: ${third}`);
+}
