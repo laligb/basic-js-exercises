9de33565bf59513e01d5482a1d01ccdd02f4596a
Lali Bibilashvili <lalibibilashvili@gmail.com>
2024-11-05 16:18:06 +0100

    exercise 1.4 level 3 done

---
+++ exercise1.4/level3.js
@@ -0,0 +1,17 @@
+/**
+ * Exercise 5
+ * Spread in Functions: Create a function that accepts three arguments.
+ * Then, create an array with three elements and call the function using the
+ * spread operator with that array.
+ */
+export function spreadFunc(arg1, arg2, arg3) {
+  console.log(arg1, arg2, arg3);
+}
+
+/**
+ * Exercise 6
+ * Merging Objects with Spread: Creates two objects with different properties.
+ * Use the spread operator to merge these two objects into a new one.
+ */
+export const objectS1 = { name: "Laura" };
+export const objectS2 = { course: "Spanish" };
--- exercise1.4/main.js
+++ exercise1.4/main.js
@@ -2,6 +2,8 @@
  * Exercise 1.4: Rest & Spread operators
  */
 import { usingRest, usingSpread } from "./level1.js";
+import { object1, object2, useRestDestructor } from "./level2.js";
+import { objectS1, objectS2, spreadFunc } from "./level3.js";
 
 // Testing Level 1
 let array1 = [2, 4, 1];
@@ -9,3 +11,15 @@ let array2 = [56, 3, 8];
 
 console.log(usingSpread(array1, array2));
 console.log(usingRest(1, 2, 3, 4));
+
+// Testing Level 2
+console.log(object1, object2);
+
+let array3 = [3, 4, 2, 9, 5, 6, 2, 9, 11, 0, 2];
+useRestDestructor(array3);
+
+// Testing Level 3
+let array4 = [3, 5, 6];
+spreadFunc(...array4);
+
+console.log({ ...objectS1, ...objectS2 });
