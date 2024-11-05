56247326f28f0afc7542cad8ef6611e72ce73fcb
Lali Bibilashvili <lalibibilashvili@gmail.com>
2024-11-05 18:44:08 +0100

    exercise 1.6 level 1 done

---
+++ exercise1.6/level1.js
@@ -0,0 +1,30 @@
+/**
+ * Exercise 1
+ * forEach: You have an array of names. Use forEach to print each name to the
+ * console: let names = ['Anna', 'Bernat', 'Clara'];
+ */
+export function printNames(names) {
+  names.forEach((element) => {
+    console.log(element);
+  });
+}
+
+/**
+ * Exercise 2
+ * for-of: You have an array of names. Use a for-of loop to print each name to
+ * the console: let names = ['Anna', 'Bernat', 'Clara'];
+ */
+export function useForOf(numbers) {
+  for (const element of numbers) {
+    console.log(element);
+  }
+}
+
+/**
+ * Exercise 3
+ * filter: You have an array of numbers. Use filter to create a new array
+ * containing only the even numbers. let numbers = [1, 2, 3, 4, 5, 6];
+ */
+export function useFilter(numbers) {
+  return numbers.filter((n) => n % 2 == 0);
+}
---
+++ exercise1.6/main.js
@@ -0,0 +1,12 @@
+/**
+ * Exercise 1.6: Array loops
+ */
+
+import { printNames, useFilter, useForOf } from "./level1.js";
+
+// Testing Level 1
+let names = ["Anna", "Bernat", "Clara"];
+printNames(names);
+useForOf(names);
+let numbers = [1, 2, 3, 4, 5, 6];
+console.log(useFilter(numbers));
